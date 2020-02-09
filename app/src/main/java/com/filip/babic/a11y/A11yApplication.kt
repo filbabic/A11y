be34dd885af7f0ec4f41d7ac67387de882ba0558
Filip Babic <fbabic96@gmail.com>
2020-02-09 10:07:30 +0100

    Initial commit

---
+++ app/src/main/java/com/filip/babic/a11y/A11yApplication.kt
@@ -0,0 +1,14 @@
+package com.filip.babic.a11y
+
+import android.app.Application
+
+/**
+ * Used to setup the A11y library, and test out the behavior.
+ */
+class A11yApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
